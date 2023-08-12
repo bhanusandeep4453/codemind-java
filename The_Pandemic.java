9242b17ff55ff5a2083d4423babd0d7851601bb3
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2023-08-12 11:36:56 +0530

    import java.util.*; public class main{     public static void main(String[] args){         Scanner x=new Scanner(System.in);         int a;         a=x.nextInt();         if(a==1){             System.out.print("0");         }else if(a==2){             System.out.print("1");         }else {             System.out.print("2");         }     } }

---
+++ The_Pandemic.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class main{
+    public static void main(String[] args){
+        Scanner x=new Scanner(System.in);
+        int a;
+        a=x.nextInt();
+        if(a==1){
+            System.out.print("0");
+        }else if(a==2){
+            System.out.print("1");
+        }else {
+            System.out.print("2");
+        }
+    }
+}
