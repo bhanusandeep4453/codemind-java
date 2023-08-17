da8da4f7cfa064c840d96b7e842244c15ab28cd2
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2023-08-17 05:57:41 +0530

    import java.util.*; public class bhanu{     public static void main(String[] args){         Scanner x=new Scanner (System.in);         int s,t,b;         s=x.nextInt();         t=x.nextInt();         b=x.nextInt();         System.out.print(s*t*b*512*2);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class bhanu{
+    public static void main(String[] args){
+        Scanner x=new Scanner (System.in);
+        int s,t,b;
+        s=x.nextInt();
+        t=x.nextInt();
+        b=x.nextInt();
+        System.out.print(s*t*b*512*2);
+    }
+}
