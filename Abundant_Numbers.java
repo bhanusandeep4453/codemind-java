9d1ff06c8ac30b74b5fd2645489572ee490c95de
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2023-08-18 10:16:50 +0530

    import java.util.*; public class bhanu{     public static void main(String[] args){         Scanner x=new Scanner (System.in);         int n,s=0,i;         n=x.nextInt();         for(i=1;i<=n/2;i++){             if(n%i==0){                 s=s+i;             }         }         if(s>n){             System.out.print("True");         }else{             System.out.print("False");         }     } }

---
+++ Abundant_Numbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class bhanu{
+    public static void main(String[] args){
+        Scanner x=new Scanner (System.in);
+        int n,s=0,i;
+        n=x.nextInt();
+        for(i=1;i<=n/2;i++){
+            if(n%i==0){
+                s=s+i;
+            }
+        }
+        if(s>n){
+            System.out.print("True");
+        }else{
+            System.out.print("False");
+        }
+    }
+}
