017b96e87dad1697a8f0111a51f687531b4c650a
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2023-08-27 19:37:15 +0530

    import java.util.*; public class bhanu{     public static void main(String[] args){         Scanner x=new Scanner(System.in);         int a,b,c,z;         a=x.nextInt();         b=x.nextInt();         c=x.nextInt();         z=(c-b);         int y=z/a;         System.out.print(y);     } }

---
+++ Can_cross_the_bridge.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class bhanu{
+    public static void main(String[] args){
+        Scanner x=new Scanner(System.in);
+        int a,b,c,z;
+        a=x.nextInt();
+        b=x.nextInt();
+        c=x.nextInt();
+        z=(c-b);
+        int y=z/a;
+        System.out.print(y);
+    }
+}
