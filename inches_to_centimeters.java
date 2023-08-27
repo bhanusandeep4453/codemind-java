2bf26f90d8c72b3f0dd6014f09f98d642e2c12cd
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2023-08-27 19:20:20 +0530

    import java.util.*; public class bhanu{     public static void main(String[] srgs){         Scanner x=new Scanner (System.in);         double i,c;         i=x.nextInt();         c=i*2.54;         System.out.printf("%.2f",c);     } }

---
+++ inches_to_centimeters.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class bhanu{
+    public static void main(String[] srgs){
+        Scanner x=new Scanner (System.in);
+        double i,c;
+        i=x.nextInt();
+        c=i*2.54;
+        System.out.printf("%.2f",c);
+    }
+}
