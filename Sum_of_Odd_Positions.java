fd35cfd6e2a975889feda9b25ee005087ad8041c
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2023-09-04 19:51:49 +0530

    import java.util.*; public class bhanu{     public static void main(String[] args){         Scanner x=new Scanner(System.in);         int i,n,os=0;         n=x.nextInt();         int[] arr=new int[n];         for(i=0;i<n;i++){             arr[i]=x.nextInt();         }         for(i=0;i<n;i++){             if(i%2!=0){                 os+=arr[i];             }         }         System.out.print(os);     } }

---
+++ Sum_of_Odd_Positions.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class bhanu{
+    public static void main(String[] args){
+        Scanner x=new Scanner(System.in);
+        int i,n,os=0;
+        n=x.nextInt();
+        int[] arr=new int[n];
+        for(i=0;i<n;i++){
+            arr[i]=x.nextInt();
+        }
+        for(i=0;i<n;i++){
+            if(i%2!=0){
+                os+=arr[i];
+            }
+        }
+        System.out.print(os);
+    }
+}
