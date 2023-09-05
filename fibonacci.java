e33705fd6fcfd160eb4f2a977ed755fa1d13cd5f
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2023-09-05 21:17:43 +0530

    import java.util.*; public class bhanu{     public static void main(String[] args){         Scanner x=new Scanner(System.in);         int i,a=0,b=1,c=0,n;         n=x.nextInt();         for(i=0;i<n;i++){             System.out.print(a+" ");             c=a+b;             a=b;             b=c;         }     } }

--- fibonacci.java
+++ fibonacci.java
@@ -2,13 +2,13 @@ import java.util.*;
 public class bhanu{
     public static void main(String[] args){
         Scanner x=new Scanner(System.in);
-        int a=0,b=1,c=0,n;
+        int i,a=0,b=1,c=0,n;
         n=x.nextInt();
-        for(int i=1;i<=n;i++){
-            System.out.print(c + " ");
+        for(i=0;i<n;i++){
+            System.out.print(a+" ");
+            c=a+b;
             a=b;
             b=c;
-            c=a+b;
         }
     }
 }
