893137830cedce2788bc5873e20e474bb84a5fa2
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2023-09-05 22:04:09 +0530

    import java.util.*; public class bhanu{     public static void main(String[] args){         Scanner x=new Scanner(System.in);         int n,i,a=0,b=1,c=0;         n=x.nextInt();         while(c<n){             c=a+b;             a=b;             b=c;         }         if(c==n)         System.out.print("True");         else         System.out.print("False");     } }

--- Fibonacci_or_not.java
+++ Fibonacci_or_not.java
@@ -2,19 +2,16 @@ import java.util.*;
 public class bhanu{
     public static void main(String[] args){
         Scanner x=new Scanner(System.in);
-        int a=0,b=1,c=0,n;
+        int n,i,a=0,b=1,c=0;
         n=x.nextInt();
-        //b=x.nextInt();
-        //c=x.nextInt();
         while(c<n){
+            c=a+b;
             a=b;
             b=c;
-            c=a+b;
         }
-        if(c==n){
-            System.out.print("True");
-        }else{
-            System.out.print("False");
-        }
+        if(c==n)
+        System.out.print("True");
+        else
+        System.out.print("False");
     }
 }
