d36519c7baf2895422cb16d2e59f085069255485
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2023-09-06 11:49:58 +0530

    import java.util.*; public class bhanu{     public static void main(String[] args){         Scanner x=new Scanner(System.in);         int n,i,c=0;         n=x.nextInt();         int[] arr=new int[n];         for(i=0;i<n;i++){             arr[i]=x.nextInt();         }         for(i=0;i<n-2;i++){             if(arr[i]%2!=0 && arr[i+2]%2!=0){                 if(arr[i+1]%2!=0){                     c+=1;                 }             }         }         System.out.print(c);     } }

---
+++ count_the_odd_numbers_inbetween_odd_numbers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class bhanu{
+    public static void main(String[] args){
+        Scanner x=new Scanner(System.in);
+        int n,i,c=0;
+        n=x.nextInt();
+        int[] arr=new int[n];
+        for(i=0;i<n;i++){
+            arr[i]=x.nextInt();
+        }
+        for(i=0;i<n-2;i++){
+            if(arr[i]%2!=0 && arr[i+2]%2!=0){
+                if(arr[i+1]%2!=0){
+                    c+=1;
+                }
+            }
+        }
+        System.out.print(c);
+    }
+}
