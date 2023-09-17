6f3b1c3ff79eeb52444b15613ba6635b1594608f
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2023-09-17 12:03:28 +0530

    import java.util.*; public class bhanu{     public static void main(String[] args){         Scanner x=new Scanner(System.in);         int n,k,c=0;         n=x.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++){             arr[i]=x.nextInt();         }         k=x.nextInt();         for(int i:arr){             if(prime(i)){                 if(i>=k){                     c++;                 }             }         }         System.out.print(c);     }     public static boolean prime(int n){         if(n==1)         return false;         for(int i=2;i<=Math.sqrt(n);i++){             if(n%i==0){                 return false;             }         }         return true;     } }

---
+++ Primes_greater_than_k.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class bhanu{
+    public static void main(String[] args){
+        Scanner x=new Scanner(System.in);
+        int n,k,c=0;
+        n=x.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=x.nextInt();
+        }
+        k=x.nextInt();
+        for(int i:arr){
+            if(prime(i)){
+                if(i>=k){
+                    c++;
+                }
+            }
+        }
+        System.out.print(c);
+    }
+    public static boolean prime(int n){
+        if(n==1)
+        return false;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
