6a54c1dd4b7b1ed436293d0f928a452cbade78ab
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2023-09-21 14:47:27 +0530

    import java.util.*; public class bhanu{     public static void main(String[] args){         Scanner x=new Scanner(System.in);         int j,n,c=0,a,b;         n=x.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++){             arr[i]=x.nextInt();         }         a=x.nextInt();         b=x.nextInt();         for(j=0;j<n;j++){             if(arr[j]<a || arr[j]>b){                 c+=arr[j];             }         }         System.out.print(c);     } }

--- Sum_of_elements_not_between_A_and_B.java
+++ Sum_of_elements_not_between_A_and_B.java
@@ -2,23 +2,19 @@ import java.util.*;
 public class bhanu{
     public static void main(String[] args){
         Scanner x=new Scanner(System.in);
-        int j,n,i,s=0,a,b;
+        int j,n,c=0,a,b;
         n=x.nextInt();
         int[] arr=new int[n];
-        for(i=0;i<n;i++)
-        {
+        for(int i=0;i<n;i++){
             arr[i]=x.nextInt();
         }
         a=x.nextInt();
         b=x.nextInt();
-        for(j=0;j<n;j++)
-        {
-            if(arr[j]<a || arr[j]>b)
-            {
-                s+=arr[j];
+        for(j=0;j<n;j++){
+            if(arr[j]<a || arr[j]>b){
+                c+=arr[j];
             }
-            //System.out.print(s);
         }
-        System.out.print(s);
+        System.out.print(c);
     }
 }
