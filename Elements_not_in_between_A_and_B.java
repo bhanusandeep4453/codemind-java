ca15555321a3d31ad5f30b93e74583e23b59d012
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2023-09-21 15:04:40 +0530

    import java.util.*; public class bhanu{     public static void main(String[] args){         Scanner x=new Scanner(System.in);         int j,n,c=0,a,b;         n=x.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++){             arr[i]=x.nextInt();         }         a=x.nextInt();         b=x.nextInt();         int f=0;         for(j=0;j<n;j++){             if(arr[j]<a || arr[j]>b){                 //c++;                 System.out.print(arr[j]+" ");                 f=1;             }         }         if(f==0){             System.out.print("-1");         }     } }

--- Elements_not_in_between_A_and_B.java
+++ Elements_not_in_between_A_and_B.java
@@ -1,25 +1,25 @@
 import java.util.*;
 public class bhanu{
     public static void main(String[] args){
-        Scanner x=new Scanner (System.in);
-        int j,n,i,s=0;
+        Scanner x=new Scanner(System.in);
+        int j,n,c=0,a,b;
         n=x.nextInt();
         int[] arr=new int[n];
-        for(i=0;i<n;i++){
+        for(int i=0;i<n;i++){
             arr[i]=x.nextInt();
         }
-        int a=x.nextInt();
-        int b=x.nextInt();
-        boolean found = false;
+        a=x.nextInt();
+        b=x.nextInt();
+        int f=0;
         for(j=0;j<n;j++){
             if(arr[j]<a || arr[j]>b){
+                //c++;
                 System.out.print(arr[j]+" ");
-                s=1;
+                f=1;
             }
         }
-        if(s==0){
+        if(f==0){
             System.out.print("-1");
         }
-        
     }
 }
