a98bb94b86605e754162beb7ca12ac021969ae51
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2023-10-13 11:22:59 +0530

    import java.util.*; public class Bhanu{     public static void main(String[] args){         Scanner x=new Scanner(System.in);         int n,f=0;         n=x.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++){             arr[i]=x.nextInt();         }         for(int i=0;i<n;i++){             if(arr[i]%2!=0){                 f=1;                 break;             }         }         if(f==1){             System.out.println("False");         }         else{             System.out.println("True");         }     } }

--- Even_Array.java
+++ Even_Array.java
@@ -1,22 +1,24 @@
 import java.util.*;
-public class bhanu{
+public class Bhanu{
     public static void main(String[] args){
         Scanner x=new Scanner(System.in);
-        int n,i,c=0;
+        int n,f=0;
         n=x.nextInt();
         int[] arr=new int[n];
-        for(i=0;i<n;i++){
+        for(int i=0;i<n;i++){
             arr[i]=x.nextInt();
         }
-        for(i=0;i<n;i++){
-            if(arr[i]%2==0){
-                c++;
+        for(int i=0;i<n;i++){
+            if(arr[i]%2!=0){
+                f=1;
+                break;
             }
         }
-        if(c==n){
-            System.out.print("True");
-        }else{
-            System.out.print("False");
+        if(f==1){
+            System.out.println("False");
+        }
+        else{
+            System.out.println("True");
         }
     }
 }
