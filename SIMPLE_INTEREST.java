32110ce47274ac0502531852654b861c4e754961
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2023-10-20 10:04:55 +0530

    import java.util.*; public class Bhanu{     public static void main(String[] args){         Scanner x=new Scanner(System.in);         int p,t,r,si;         p=x.nextInt();         t=x.nextInt();         r=x.nextInt();         si=(p*t*r)/100;         System.out.print(si);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Bhanu{
+    public static void main(String[] args){
+        Scanner x=new Scanner(System.in);
+        int p,t,r,si;
+        p=x.nextInt();
+        t=x.nextInt();
+        r=x.nextInt();
+        si=(p*t*r)/100;
+        System.out.print(si);
+    }
+}
