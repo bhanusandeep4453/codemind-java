8fd4174657cdb51ed0306ec92ec968467d21ba0b
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2023-10-20 09:48:34 +0530

    import java.util.*; public class Bhanu{     public static void main(String[] args){         Scanner x=new Scanner (System.in);         int c;         double f;         c=x.nextInt();         f=(c*1.8)+32;         System.out.printf("%.2f",f);     } }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Bhanu{
+    public static void main(String[] args){
+        Scanner x=new Scanner (System.in);
+        int c;
+        double f;
+        c=x.nextInt();
+        f=(c*1.8)+32;
+        System.out.printf("%.2f",f);
+    }
+}
